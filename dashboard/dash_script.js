4901b643349825ad5f92fe9869d01be820dccf22
Stoklas Nac <ns1844@student.uni-lj.si>
2016-11-03 09:43:25 +0000

    dodan dashboard html ipd.

---
+++ dashboard/dash_script.js
@@ -0,0 +1,8 @@
+var c = document.getElementById("canv");
+var ctx = c.getContext("2d");
+ctx.moveTo(15,15);
+ctx.lineTo(15,130);
+ctx.stroke();
+ctx.moveTo(15,130);
+ctx.lineTo(270, 130);
+ctx.stroke();
