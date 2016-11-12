b2d789439ebce6444c30d31db8d8254efc017f74
Stoklas Nac <ns1844@student.uni-lj.si>
2016-11-12 22:00:03 +0000

    Dodana obrazca za vpis ter registracijo. + style.

--- dashboard/dash_script.js
+++ dashboard/dash_script.js
@@ -1,8 +1,40 @@
-var c = document.getElementById("canv");
-var ctx = c.getContext("2d");
-ctx.moveTo(15,15);
-ctx.lineTo(15,130);
-ctx.stroke();
-ctx.moveTo(15,130);
-ctx.lineTo(270, 130);
-ctx.stroke();
+narisi_prvi_del();
+
+function narisi_prvi_del(){
+    var c = document.getElementById("canv");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(15,15);
+    ctx.lineTo(15,130);
+    ctx.stroke();
+    ctx.moveTo(15,130);
+    ctx.lineTo(270, 130);
+    ctx.stroke();
+}
+
+function pregled_f(){
+
+}
+
+function prilivi_f(){
+
+}
+
+function odlivi_f(){
+
+}
+
+function cilji_f(){
+
+}
+
+function viri_dohodka_f(){
+
+}
+
+function nastavitve_f(){
+
+}
+
+function moj_racun_f(){
+
+}
